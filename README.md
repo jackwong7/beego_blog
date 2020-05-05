d687390911422674f8dbc5c9128108a673b413bb
JackWong <wangchangat@163.com>
2020-05-05 17:34:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# beego_Blog
+
+基于Go语言和beego框架 前端使用layui 布局 开发的个人博客系统
+
+## 编译安装说明：
+
+
+1 . 下载安装
+
+    $ go get github.com/jackwong7/beego_blog
+
+2 . 加入数据库
+
+   mysql 新建blog数据库把根目录 beego_blog.sql 导入
+
+3 . 修改 app.conf 配置
+
+    #MYSQL地址
+    dbhost = localhost
+
+    #MYSQL端口
+    dbport = 3306
+
+    #MYSQL用户名
+    dbuser = root
+
+    #MYSQL密码
+    dbpassword =
+
+    #MYSQL数据库名称
+    dbname = beego_blog
+
+    #MYSQL表前缀
+    dbprefix = tb_
+
+ 6 . 运行
+
+    cd 到 beego_blog 目录 执行
+    $ bee run
+
+ 7 . 浏览器演示
+
+http://localhost:8080 (前台)
+
+http://localhost:8080/admin/login (后台)
+
---
+++ beego_blog.sql
@@ -0,0 +1,237 @@
+-- MySQL dump 10.13  Distrib 5.7.24, for Win64 (x86_64)
+--
+-- Host: localhost    Database: beego_blog
+-- ------------------------------------------------------
+-- Server version	5.7.24
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `beego_blog`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `beego_blog` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `beego_blog`;
+
+--
+-- Table structure for table `tb_category`
+--
+
+DROP TABLE IF EXISTS `tb_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_category`
+--
+
+LOCK TABLES `tb_category` WRITE;
+/*!40000 ALTER TABLE `tb_category` DISABLE KEYS */;
+INSERT INTO `tb_category` VALUES (6,'PHP','2020-05-05 07:16:25','2020-05-05 07:16:25'),(7,'GO','2020-05-05 07:16:32','2020-05-05 07:16:32');
+/*!40000 ALTER TABLE `tb_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_comment`
+--
+
+DROP TABLE IF EXISTS `tb_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  `content` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ip` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_comment`
+--
+
+LOCK TABLES `tb_comment` WRITE;
+/*!40000 ALTER TABLE `tb_comment` DISABLE KEYS */;
+INSERT INTO `tb_comment` VALUES (10,'234','234234','2020-05-05 07:39:10','127.0.0.1',13);
+/*!40000 ALTER TABLE `tb_comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_config`
+--
+
+DROP TABLE IF EXISTS `tb_config`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_config` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `value` text CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_config`
+--
+
+LOCK TABLES `tb_config` WRITE;
+/*!40000 ALTER TABLE `tb_config` DISABLE KEYS */;
+INSERT INTO `tb_config` VALUES (1,'title','JackWong Blog'),(2,'url','http://jackwong.xyz'),(5,'keywords','JackWong Blog'),(6,'description','JackWong Blog'),(7,'email','wangchangat@gmail.com'),(9,'timezone','8'),(11,'start','1'),(12,'qq','80526502');
+/*!40000 ALTER TABLE `tb_config` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_post`
+--
+
+DROP TABLE IF EXISTS `tb_post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '下载地址',
+  `content` mediumtext,
+  `tags` varchar(100) NOT NULL,
+  `views` mediumint(9) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `is_top` tinyint(4) NOT NULL DEFAULT '0',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `category_id` int(11) NOT NULL,
+  `types` tinyint(4) DEFAULT NULL COMMENT '1. 文章 0 下载',
+  `info` varchar(500) CHARACTER SET utf8 DEFAULT NULL COMMENT '简介',
+  `image` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_post`
+--
+
+LOCK TABLES `tb_post` WRITE;
+/*!40000 ALTER TABLE `tb_post` DISABLE KEYS */;
+INSERT INTO `tb_post` VALUES (11,1,'如何学习PHP','','<p>测试内容</p><p>撒旦发射点</p><p>sdfsd</p><p><br/></p><p>sdfsd</p>','学习',0,0,1,'2020-05-05 09:12:59','2020-05-05 09:12:59',7,1,'学习好php','');
+/*!40000 ALTER TABLE `tb_post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_tag`
+--
+
+DROP TABLE IF EXISTS `tb_tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_tag` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '标签名',
+  `count` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '使用次数',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_tag`
+--
+
+LOCK TABLES `tb_tag` WRITE;
+/*!40000 ALTER TABLE `tb_tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_tag_post`
+--
+
+DROP TABLE IF EXISTS `tb_tag_post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_tag_post` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `tag_id` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '标签id',
+  `post_id` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '内容id',
+  PRIMARY KEY (`id`),
+  KEY `tagid` (`tag_id`),
+  KEY `postid` (`post_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_tag_post`
+--
+
+LOCK TABLES `tb_tag_post` WRITE;
+/*!40000 ALTER TABLE `tb_tag_post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_tag_post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_user`
+--
+
+DROP TABLE IF EXISTS `tb_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `login_count` int(11) DEFAULT NULL,
+  `last_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `last_ip` varchar(200) DEFAULT 'current_timestamp()',
+  `state` tinyint(4) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+LOCK TABLES `tb_user` WRITE;
+/*!40000 ALTER TABLE `tb_user` DISABLE KEYS */;
+INSERT INTO `tb_user` VALUES (1,'jack','a82cf535acefda50f3472d2ee244162f','',24,'2020-05-05 07:18:10','[',0,NULL,'2020-05-05 07:18:10');
+/*!40000 ALTER TABLE `tb_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-05 17:21:57
