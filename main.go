8e30c9f549db14ccc344103c9e82cb2aeefe6b29
JackWong <wangchangat@163.com>
2020-05-07 18:01:19 +0800

    session 加入 redis

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	_ "github.com/astaxie/beego/session/redis"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jackwong7/beego_blog/models"
 	_ "github.com/jackwong7/beego_blog/routers"
@@ -9,9 +10,9 @@ import (
 
 func init() {
 	models.Init()
-	//beego.BConfig.WebConfig.Session.SessionOn = true
-	//beego.BConfig.WebConfig.Session.SessionProvider = "redis"
-	//beego.BConfig.WebConfig.Session.SessionProviderConfig = "127.0.0.1:6379"
+	beego.BConfig.WebConfig.Session.SessionOn = true
+	beego.BConfig.WebConfig.Session.SessionProvider = "redis"
+	beego.BConfig.WebConfig.Session.SessionProviderConfig = "127.0.0.1:6379"
 }
 
 func main() {
