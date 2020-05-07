508e092e9c597dc2bf628dae4e24dc65563693f4
JackWong <wangchangat@163.com>
2020-05-07 17:59:37 +0800

    session 移除 redis

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/astaxie/beego"
-	_ "github.com/astaxie/beego/session/redis"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jackwong7/beego_blog/models"
 	_ "github.com/jackwong7/beego_blog/routers"
@@ -10,9 +9,9 @@ import (
 
 func init() {
 	models.Init()
-	beego.BConfig.WebConfig.Session.SessionOn = true
-	beego.BConfig.WebConfig.Session.SessionProvider = "redis"
-	beego.BConfig.WebConfig.Session.SessionProviderConfig = "127.0.0.1:6379"
+	//beego.BConfig.WebConfig.Session.SessionOn = true
+	//beego.BConfig.WebConfig.Session.SessionProvider = "redis"
+	//beego.BConfig.WebConfig.Session.SessionProviderConfig = "127.0.0.1:6379"
 }
 
 func main() {
