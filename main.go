84c1996d59361836f54a2b21fc1b17cfd86a4ff1
JackWong <wangchangat@163.com>
2020-05-08 14:32:24 +0800

    优化内存

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"github.com/astaxie/beego"
 	_ "github.com/astaxie/beego/session/redis"
 	_ "github.com/go-sql-driver/mysql"
@@ -13,6 +14,7 @@ func init() {
 	beego.BConfig.WebConfig.Session.SessionOn = true
 	beego.BConfig.WebConfig.Session.SessionProvider = "redis"
 	beego.BConfig.WebConfig.Session.SessionProviderConfig = "127.0.0.1:6379"
+	gob.Register(models.User{})
 }
 
 func main() {
--- models/post.go
+++ models/post.go
@@ -27,6 +27,22 @@ type Post struct {
 	Image      string
 }
 
+type PostView struct {
+	Id         int
+	Title      string
+	Tags       string
+	Views      int
+	CategoryId int
+	Info       string
+	Image      string
+	Updated    time.Time
+}
+
+type PostHotView struct {
+	Id    int
+	Title string
+}
+
 func (m *Post) TableName() string {
 	return TableName("post")
 }
@@ -40,8 +56,8 @@ type QueryField struct {
 
 type Postlists struct {
 	Keyword  string
-	List     []*Post
-	Hosts    []*Post
+	List     []*PostView
+	Hosts    []*PostHotView
 	Page     int
 	Pagesize int
 	Count    int64
@@ -81,17 +97,37 @@ func GetPosts(queryField QueryField, o orm.Ormer) Postlists {
 	if queryField.Keyword != "" {
 		query = query.Filter("title__icontains", queryField.Keyword)
 	}
-	query.OrderBy("-views").Limit(10, 0).All(&hosts)
+	query.OrderBy("-views").Limit(10, 0).All(&hosts, "id", "title")
 
 	if queryField.ActionName == "home" {
 		query = query.Filter("is_top", 1)
 	}
 	count, _ = query.Count()
-	query.OrderBy("-created").Limit(pagesize, offset).All(&list)
+	query.OrderBy("-created").Limit(pagesize, offset).All(&list, "id", "title", "image", "tags", "views", "info", "updated")
+	var writeList []*PostView
+	for _, v := range list {
+		writeList = append(writeList, &PostView{
+			Id:         v.Id,
+			Title:      v.Title,
+			Tags:       v.Tags,
+			Views:      v.Views,
+			CategoryId: v.CategoryId,
+			Info:       v.Info,
+			Image:      v.Image,
+			Updated:    v.Updated,
+		})
+	}
+	var writehots []*PostHotView
+	for _, v := range list {
+		writehots = append(writehots, &PostHotView{
+			Id:    v.Id,
+			Title: v.Title,
+		})
+	}
 	postlists = Postlists{
 		Keyword:  queryField.Keyword,
-		List:     list,
-		Hosts:    hosts,
+		List:     writeList,
+		Hosts:    writehots,
 		Page:     queryField.Page,
 		Pagesize: pagesize,
 		Count:    count,
