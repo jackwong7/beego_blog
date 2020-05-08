9df6d035c2126c3039b328d5a641220237213f62
JackWong <wangchangat@163.com>
2020-05-08 16:23:39 +0800

    去掉session入redis

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"encoding/gob"
 	"github.com/astaxie/beego"
-	_ "github.com/astaxie/beego/session/redis"
+	//_ "github.com/astaxie/beego/session/redis"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jackwong7/beego_blog/models"
 	_ "github.com/jackwong7/beego_blog/routers"
@@ -11,10 +10,10 @@ import (
 
 func init() {
 	models.Init()
-	beego.BConfig.WebConfig.Session.SessionOn = true
-	beego.BConfig.WebConfig.Session.SessionProvider = "redis"
-	beego.BConfig.WebConfig.Session.SessionProviderConfig = "127.0.0.1:6379"
-	gob.Register(models.User{})
+	//beego.BConfig.WebConfig.Session.SessionOn = true
+	//beego.BConfig.WebConfig.Session.SessionProvider = "redis"
+	//beego.BConfig.WebConfig.Session.SessionProviderConfig = "127.0.0.1:6379"
+	//gob.Register(models.User{})
 }
 
 func main() {
